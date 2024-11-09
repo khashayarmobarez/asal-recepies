52e5cd48cfccbb4b7c64368473d31ac1a7c5cfdd
khashayar <khashayarmobarezbusiness@gmail.com>
2024-11-09 19:39:12 +0330

    forgot to commit

--- components/templates/RecepieDetailsPage.js
+++ components/templates/RecepieDetailsPage.js
@@ -15,10 +15,27 @@ const RecepieDetailsPageComp = ({data}) => {
     } = data
 
     return (
-        <div className=' flex flex-col w-[90%]'>
-            <Image src={'/images/${id}.jpeg'} alt={name} />
-            <h3>{name}</h3>
-            <p>{introduction}</p>
+        <div className=' flex flex-col w-[90%] items-center'>
+            {/* <Image src={`/images/${id}.jpeg`} alt={name} /> */}
+            <div className='w-full flex flex-col' id='banner'>
+                <h3>{name}</h3>
+                <p>{details[0].Cuisine}</p>
+                <p>{}</p>
+            </div>
+
+            <div id='introduction' className='w-full flex flex-col'>
+                <p>{introduction}</p>
+            </div>
+
+            <div id='details' className='flex flex-col items-center'>
+                <h2>Details</h2>
+                <ul>
+                    {details.map((details, index) => 
+                        <li key={index}>test</li>
+                    )}
+                </ul>
+            </div>
+
         </div>
     );
 };
